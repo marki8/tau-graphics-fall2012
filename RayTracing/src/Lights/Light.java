fa4f187d083eff4ece7eae9f8f5058b5e0f1f8a1
asafbruner@gmail.com <asafbruner@gmail.com@c8402f21-b21d-3c62-db27-ce5478d24b99>
2011-12-24 19:22:21 +0000

    A few bug fixes

--- RayTracing/src/Lights/Light.java
+++ RayTracing/src/Lights/Light.java
@@ -34,7 +34,11 @@ public abstract class Light {
 		double NL = N.dotProduct(L.normalize());
 		Vector Ka = hit.getMinIntPoint().getGeom().getSurface().getMtlAmbient();
 		
+		if(NL<0) NL*=-1;
+		if(VR<0) VR*=-1;
+		
 		// TODO: add emition component 
+		// TODO: take in consideration the distance of the lighting 
 		double colorX = (Ka.getDoubleX() * scene.getAmbientLight().getDoubleX()) + intensity.getX()*((Kd.getDoubleX()*NL) + (Ks.getDoubleX()*Math.pow(VR, n)));
 		double colorY = (Ka.getDoubleY() * scene.getAmbientLight().getDoubleY()) + intensity.getY()*((Kd.getDoubleY()*NL) + (Ks.getDoubleY()*Math.pow(VR, n)));
 		double colorZ = (Ka.getDoubleZ() * scene.getAmbientLight().getDoubleZ()) + intensity.getZ()*((Kd.getDoubleZ()*NL) + (Ks.getDoubleZ()*Math.pow(VR, n)));
