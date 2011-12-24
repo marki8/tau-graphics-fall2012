f08aca079daad9005eb7a82c893a9c9ef02ae6b2
avivgoll <avivgoll@gmail.com>
2011-12-24 11:08:09 +0000

    sphere done :)

--- RayTracing/src/GeometricPrimitives/Sphere.java
+++ RayTracing/src/GeometricPrimitives/Sphere.java
@@ -19,17 +19,33 @@ public class Sphere extends GeometricPrimitive {
 
 	@Override
 	public IntersectioPoint getIntersection(Ray r) {
+
+		// Algorithm from lecture slides 4a
+		Vector L = center.substract(r.getOrigin());
+		double t_CA = L.dotProduct(r.getDirection());
+		if ( t_CA < 0 ) return null;
+		double d2 = L.dotProduct(L) - t_CA*t_CA;
+		double r2 = radius*radius;
+		if ( d2 > r2) return null;
+		double t_hc = Math.sqrt(r2 - d2);
 		
-		// is there an intersection?
-		// find the distance between the ray and the center 
-		double d = r.getDirection().dotProduct(r.getOrigin());
-		double distance = (d + Math.abs( r.getDirection().dotProduct(center) )) / r.getDirection().length();
-		if ( distance > radius )
-			return null;
+		Vector int1 = r.getOrigin().add(r.getDirection().scalarMult(t_CA-t_hc));
+		Vector int2 = r.getOrigin().add(r.getDirection().scalarMult(t_CA+t_hc));
 		
-		
-		
-		return null;
+		if ( int1.distance(r.getOrigin()) < int2.distance(r.getOrigin()))
+			return new IntersectioPoint(this, int1, int1.substract(center));
+		else
+			return new IntersectioPoint(this, int2, int2.substract(center));
+//		// is there an intersection?
+//		// find the distance between the ray and the center 
+//		double d = r.getDirection().dotProduct(r.getOrigin());
+//		double distance = (d + Math.abs( r.getDirection().dotProduct(center) )) / r.getDirection().length();
+//		if ( distance > radius )
+//			return null;
+//		
+//		
+//		
+//		return null;
 	}
 
 
