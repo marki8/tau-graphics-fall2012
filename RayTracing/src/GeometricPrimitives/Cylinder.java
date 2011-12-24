ecf791a5b7fd5479d52e6ad4d5aa5f252a4f0ef0
avivgoll@gmail.com <avivgoll@gmail.com@c8402f21-b21d-3c62-db27-ce5478d24b99>
2011-12-24 10:29:14 +0000

    cylinder stub

--- RayTracing/src/GeometricPrimitives/Cylinder.java
+++ RayTracing/src/GeometricPrimitives/Cylinder.java
@@ -6,10 +6,78 @@ import raytracer.Vector;
 
 public class Cylinder extends GeometricPrimitive {
 
+	Vector start;
+	Vector direction;
+	double length;
+	double radius;
+	
+	
+	
+	public Cylinder(Vector start, Vector direction, double length, double radius) {
+		super();
+		this.start = start;
+		this.direction = direction;
+		this.length = length;
+		this.radius = radius;
+	}
+
+
+
 	@Override
 	public IntersectioPoint getIntersection(Ray r) {
-		// TODO Auto-generated method stub
+//		if ( Math.abs( r.getDirection().dotProduct(direction) ) == 1.0 ) { // parallel
+//			return null; // in this case, no intersection
+//			
+//		}
 		return null;
+	}
+
+
+
+	public Vector getStart() {
+		return start;
+	}
+
+
+
+	public void setStart(Vector start) {
+		this.start = start;
+	}
+
+
+
+	public Vector getDirection() {
+		return direction;
+	}
+
+
+
+	public void setDirection(Vector direction) {
+		this.direction = direction;
+	}
+
+
+
+	public double getLength() {
+		return length;
+	}
+
+
+
+	public void setLength(double length) {
+		this.length = length;
+	}
+
+
+
+	public double getRadius() {
+		return radius;
+	}
+
+
+
+	public void setRadius(double radius) {
+		this.radius = radius;
 	}
 
 }
