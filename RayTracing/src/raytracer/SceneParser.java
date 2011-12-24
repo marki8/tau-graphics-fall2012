0facabc1f0961d492e3ccd6d70a739fe8336577e
asafbruner@gmail.com <asafbruner@gmail.com@c8402f21-b21d-3c62-db27-ce5478d24b99>
2011-12-24 13:01:15 +0000

    SceneParser with illegal input handeling

--- RayTracing/src/raytracer/SceneParser.java
+++ RayTracing/src/raytracer/SceneParser.java
@@ -27,7 +27,7 @@ public class SceneParser {
 	
 	
 	public SceneParser() {
-		//this.scene = m_scene;
+
 	}
 
 	public Scene parse(BufferedReader br, int height, int width) throws IOException {
@@ -38,20 +38,20 @@ public class SceneParser {
 		sceneHeight = height;
 		sceneWidth = width;
 		
-		// TODO: deal with cases of illegal input
-		
 		while((line = br.readLine()) != null) {
 			if(line.length() == 0){
 				continue;
 			}
 			else if (line.charAt(line.length() - 1) == ':') {
 				obj = line.substring(0, line.length() - 1);
-				//geoList.add((GeometricPrimitive) addObject(obj));
 				addObject(obj);
-				System.out.println("object: " + obj); //????
+				//System.out.println("object: " + obj);
 			}
 			else {
 				int equalsLoc = line.indexOf('=');
+				if(equalsLoc == -1){
+					throw new Error("Missing '=' sign");
+				}
 				int tmpEqualsLoc = equalsLoc; 
 				int firstCharLocationBeforEq, firstCharLocationAfterEq;
 				tmpEqualsLoc--;
@@ -68,19 +68,13 @@ public class SceneParser {
 
 				String[] args = line.substring(firstCharLocationAfterEq).trim().toLowerCase().split("\\s+");
 				
-				if(obj == null){
-					// ????
-				}
-				
 				addParams(param, args, obj);
 					
-				
-				
-				System.out.print("param : " + param + " value: "); //????
+				/*System.out.print("param : " + param + " value: ");
 				for(int i=0; i<args.length; i++){
 					System.out.print(args[i] + " ");
 				}
-				System.out.println();
+				System.out.println();*/
 			}
 			
 		}
@@ -90,12 +84,16 @@ public class SceneParser {
 	private void addParams(String param, String[] args, String obj) {
 		
 		double val=0;
+		boolean unknownParam = false, notMtl = false, notShape = false;
 		Vector valVec = new Vector(0,0,0);
 		if(args.length == 1) {
 			val = Double.parseDouble(args[0]);
 		}
 		else if(args.length == 3){
 			valVec = new Vector(Double.parseDouble(args[0]), Double.parseDouble(args[1]),Double.parseDouble(args[2]));
+		}
+		else{
+			throw new Error("Wrong number of parameters");
 		}
 		
 		if(obj.equals("scene")){
@@ -104,6 +102,7 @@ public class SceneParser {
 			else if(param.equals("ambient-light")) scene.setAmbientLight(valVec);
 			else if(param.equals("super-samp-width")) scene.setSuperSampling((int) val);
 			else if(param.equals("use-acceleration")) scene.setUseAcceleration((int) val);
+			else unknownParam = true;
 		}
 		else if(obj.equals("camera")){
 			if(param.equals("eye")) scene.getCam().setEye(valVec);
@@ -112,60 +111,66 @@ public class SceneParser {
 			else if(param.equals("up-direction")) scene.getCam().setUpDirection(valVec);
 			else if(param.equals("screen-dist")) scene.getCam().setScreenDist(val);
 			else if(param.equals("screen-width")) scene.getCam().setScreenWidth(val);
-			
+			else unknownParam = true;
 		}
 		else if(obj.equals("light-directed")){
 			if(param.equals("color")) scene.lightList.get(scene.lightList.size()-1).setIntensity(valVec);
-			if(param.equals("direction")) ((ParallelDirectionalLight)scene.lightList.get(scene.lightList.size()-1)).setDirection(valVec);
-			
+			else if(param.equals("direction")) ((ParallelDirectionalLight)scene.lightList.get(scene.lightList.size()-1)).setDirection(valVec);
+			else unknownParam = true;
 		}
 		else if(obj.equals("light-point")){
 			if(param.equals("color")) scene.lightList.get(scene.lightList.size()-1).setIntensity(valVec);
-			if(param.equals("pos")) ((PointLight)scene.lightList.get(scene.lightList.size()-1)).setPosition(valVec);
-			if(param.equals("attenuation")) ((PointLight)scene.lightList.get(scene.lightList.size()-1)).setAttenuation(valVec);
+			else if(param.equals("pos")) ((PointLight)scene.lightList.get(scene.lightList.size()-1)).setPosition(valVec);
+			else if(param.equals("attenuation")) ((PointLight)scene.lightList.get(scene.lightList.size()-1)).setAttenuation(valVec);
+			else unknownParam = true;
 		}
 		if(obj.equals("disc") || obj.equals ("sphere") || obj.equals ("box") || obj.equals ("cylinder") || obj.equals ("rectangle")){
 			if(param.equals("mtl-type")) scene.geoList.get(scene.geoList.size()-1).getSurface().setSurfaceType(args[0]);
-			if(param.equals("mtl-diffuse")){
-				GeometricPrimitive g = scene.geoList.get(scene.geoList.size()-1);
-				Surface s = g.getSurface();
-				s.setMtlDiffuse(valVec);
-			}
-			if(param.equals("mtl-specular")) scene.geoList.get(scene.geoList.size()-1).getSurface().setMtlSpecular(valVec);
-			if(param.equals("mtl-ambient")) scene.geoList.get(scene.geoList.size()-1).getSurface().setMtlAmbient(valVec);
-			if(param.equals("mtl-emission")) scene.geoList.get(scene.geoList.size()-1).getSurface().setMtlEmission(valVec);
-			if(param.equals("mtl-shininess")) scene.geoList.get(scene.geoList.size()-1).getSurface().setMtlShininess(val);
-			if(param.equals("checkers-size")) scene.geoList.get(scene.geoList.size()-1).getSurface().setCheckersSize((int)val);
-			if(param.equals("checkers-diffuse1")) scene.geoList.get(scene.geoList.size()-1).getSurface().setCheckersDiffuse1(valVec);
-			if(param.equals("checkers-diffuse2")) scene.geoList.get(scene.geoList.size()-1).getSurface().setCheckersDiffuse2(valVec);
-			if(param.equals("texture")) scene.geoList.get(scene.geoList.size()-1).getSurface().setTexture(args[0]);
-			if(param.equals("reflectance")) scene.geoList.get(scene.geoList.size()-1).getSurface().setReflectance(val);
+			else if(param.equals("mtl-diffuse")) scene.geoList.get(scene.geoList.size()-1).getSurface().setMtlDiffuse(valVec);
+			else if(param.equals("mtl-specular")) scene.geoList.get(scene.geoList.size()-1).getSurface().setMtlSpecular(valVec);
+			else if(param.equals("mtl-ambient")) scene.geoList.get(scene.geoList.size()-1).getSurface().setMtlAmbient(valVec);
+			else if(param.equals("mtl-emission")) scene.geoList.get(scene.geoList.size()-1).getSurface().setMtlEmission(valVec);
+			else if(param.equals("mtl-shininess")) scene.geoList.get(scene.geoList.size()-1).getSurface().setMtlShininess(val);
+			else if(param.equals("checkers-size")) scene.geoList.get(scene.geoList.size()-1).getSurface().setCheckersSize((int)val);
+			else if(param.equals("checkers-diffuse1")) scene.geoList.get(scene.geoList.size()-1).getSurface().setCheckersDiffuse1(valVec);
+			else if(param.equals("checkers-diffuse2")) scene.geoList.get(scene.geoList.size()-1).getSurface().setCheckersDiffuse2(valVec);
+			else if(param.equals("texture")) scene.geoList.get(scene.geoList.size()-1).getSurface().setTexture(args[0]);
+			else if(param.equals("reflectance")) scene.geoList.get(scene.geoList.size()-1).getSurface().setReflectance(val);
+			else notMtl = true;
 		}
 		if(obj.equals("rectangle")){
 			if(param.equals("p0")) ((Rectangle)scene.geoList.get(scene.geoList.size()-1)).setP0(valVec);
-			if(param.equals("p1")) ((Rectangle)scene.geoList.get(scene.geoList.size()-1)).setP1(valVec);
-			if(param.equals("p2")) ((Rectangle)scene.geoList.get(scene.geoList.size()-1)).setP2(valVec);
+			else if(param.equals("p1")) ((Rectangle)scene.geoList.get(scene.geoList.size()-1)).setP1(valVec);
+			else if(param.equals("p2")) ((Rectangle)scene.geoList.get(scene.geoList.size()-1)).setP2(valVec);
+			else notShape = true;
 		}
 		if(obj.equals("disc")){
 			if(param.equals("center")) ((Circle)scene.geoList.get(scene.geoList.size()-1)).setCenter(valVec);
-			if(param.equals("normal")) ((Circle)scene.geoList.get(scene.geoList.size()-1)).setNormal(valVec);
-			if(param.equals("radius")) ((Circle)scene.geoList.get(scene.geoList.size()-1)).setRadius(val);
+			else if(param.equals("normal")) ((Circle)scene.geoList.get(scene.geoList.size()-1)).setNormal(valVec);
+			else if(param.equals("radius")) ((Circle)scene.geoList.get(scene.geoList.size()-1)).setRadius(val);
+			else notShape = true;
 		}
 		if(obj.equals("sphere")){
 			if(param.equals("center")) ((Sphere)scene.geoList.get(scene.geoList.size()-1)).setCenter(valVec);
-			if(param.equals("radius")) ((Sphere)scene.geoList.get(scene.geoList.size()-1)).setRadius(val);
+			else if(param.equals("radius")) ((Sphere)scene.geoList.get(scene.geoList.size()-1)).setRadius(val);
+			else notShape = true;
 		}
 		if(obj.equals("box")){
 			if(param.equals("p0")) ((Box)scene.geoList.get(scene.geoList.size()-1)).setP0(valVec);
-			if(param.equals("p1")) ((Box)scene.geoList.get(scene.geoList.size()-1)).setP1(valVec);
-			if(param.equals("p2")) ((Box)scene.geoList.get(scene.geoList.size()-1)).setP2(valVec);
-			if(param.equals("p3")) ((Box)scene.geoList.get(scene.geoList.size()-1)).setP3(valVec);
+			else if(param.equals("p1")) ((Box)scene.geoList.get(scene.geoList.size()-1)).setP1(valVec);
+			else if(param.equals("p2")) ((Box)scene.geoList.get(scene.geoList.size()-1)).setP2(valVec);
+			else if(param.equals("p3")) ((Box)scene.geoList.get(scene.geoList.size()-1)).setP3(valVec);
+			else notShape = true;
 		}
-		else if(obj.equals("cylinder")){
+		if(obj.equals("cylinder")){
 			if(param.equals("start")) ((Cylinder)scene.geoList.get(scene.geoList.size()-1)).setStart(valVec);
-			if(param.equals("direction")) ((Cylinder)scene.geoList.get(scene.geoList.size()-1)).setDirection(valVec);
-			if(param.equals("length")) ((Cylinder)scene.geoList.get(scene.geoList.size()-1)).setLength(val);
-			if(param.equals("radius")) ((Cylinder)scene.geoList.get(scene.geoList.size()-1)).setRadius(val);
+			else if(param.equals("direction")) ((Cylinder)scene.geoList.get(scene.geoList.size()-1)).setDirection(valVec);
+			else if(param.equals("length")) ((Cylinder)scene.geoList.get(scene.geoList.size()-1)).setLength(val);
+			else if(param.equals("radius")) ((Cylinder)scene.geoList.get(scene.geoList.size()-1)).setRadius(val);
+			else notShape = true;
+		}
+		if(unknownParam || (notShape && notMtl)){
+			throw new Error("Unknown param: " + param);
 		}
 		
 	}
@@ -175,15 +180,15 @@ public class SceneParser {
 		else if(obj.equals("camera")) scene.setCam(new Camera());
 		else if(obj.equals("light-directed")) scene.addLight(new ParallelDirectionalLight());
 		else if(obj.equals("light-point")) scene.addLight(new PointLight());
-//		//if(obj.equals("light-area")) return new ;
+		//if(obj.equals("light-area")) return new ;
 		else if(obj.equals("rectangle")) scene.addGeomObject(new Rectangle());
-//		//if(obj.equals("disc")) return new ;
+		else if(obj.equals("Circle")) scene.addGeomObject(new Circle());
 		else if(obj.equals("sphere")) scene.addGeomObject(new Sphere());
 		else if(obj.equals("box")) scene.addGeomObject(new Box());
 		else if(obj.equals("cylinder")) scene.addGeomObject(new Cylinder());
-		
-		
-		// ?????? IF NOT RECOGNIZED THROW ERROR?
+		else{
+			throw new Error("Unknown object: " + obj);
+		}
 	}
 
 
